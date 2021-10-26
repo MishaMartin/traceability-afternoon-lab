668b48d70fac4db746481c0cf6ae8edc88dcda58
Misha Martin <mishanicolemartin@gmail.com>
2021-10-26 16:11:49 -0600

    Initial commit

--- index.js
+++ index.js
@@ -20,10 +20,19 @@ app.get('/style', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/styles.css'))
 })
 
-app.get('/api/commands', (req,res) => {try {
+app.get('/api/commands', (req,res) =>
+{console.log("hit")
+    try {
     nonExistentFunction();
   } catch (error) {
     rollbar.error(error)}})
+
+// app.post('/api/commander', (req, res) => {
+//     let {name} = req.body
+//     name = name.trim()
+
+//     const index = students.findIndex(studentName =>studentName === name)
+// })
 
     app.use(rollbar.errorHandler())
 
